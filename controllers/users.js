6c9cf6679c53f0195e08dca94c1925a33c04401d
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-04-17 21:36:17 -0500

    Middleware completado y profile completo

---
+++ controllers/users.js
@@ -0,0 +1,56 @@
+import db from '../models'
+//Así se manda llamar a la bd
+
+const User=db.User; //db trae todas las de BD
+
+const createUser = (request,response) => {
+
+    User.create({
+        first_name:request.body.first_name,
+        lastname:request.body.lastname,
+        email:request.body.email,
+        password:request.body.password,
+        phone_number:request.body.phone_number,
+        type:1
+    }).then((user)=>{
+        response.json(user)
+    }).catch((err)=>{
+        response.status(400).json(err);
+
+    });
+
+}
+
+const viewUser = (request,response) => {
+
+    User.findOne({
+        attributes: ['first_name', 'lastname','email','password','phone_number','type'],
+        where:{
+            email:request.user.email
+        }
+    }).then((user)=>{
+        response.json(user)
+    }).catch((err)=>{
+        response.status(400).json(err);
+
+    });
+
+}
+
+const updateUser = (request,response) => {
+
+    User.findAll({
+        where:{
+            first_name:request.body.first_name
+        }
+    }).then((user)=>{
+
+        response.json(user)
+    }).catch((err)=>{
+        response.status(400).json(err);
+    }); 
+
+}
+
+export{
+    createUser,viewUser,updateUser}
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import {testApi} from '../controllers';
+import{createUser, viewUser, updateUser} from '../controllers/users'
+import{authenticationMiddleware} from '../middlewares'
 
 
 const router = express.Router();
@@ -7,6 +9,12 @@ const router = express.Router();
 
 
 router.get('/test',testApi);
+
+//Rutas de usuario
+
+router.post('/users',createUser); //Ruta para crear usuarios
+router.get('/users/profile', authenticationMiddleware, viewUser); //Ruta para ver usuarios
+router.put('/users/profile', authenticationMiddleware, updateUser); //Ruta para actualizar usuarios
 
 
 export default router;
