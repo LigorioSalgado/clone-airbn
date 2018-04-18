43109a5e459034defb13a4ac2f108452c5e6ea9f
Javier <cjjj@tutanota.com>
2018-04-17 21:30:20 -0500

    Empieza maquetación de formulario de perfil

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './profile.css';
+import { Col, Button, Form, FormGroup, Label, Input, FormText, Container, Row } from 'reactstrap';
+
+
 //import para  traer el componenet raiz de react
 
 //Se crea clase con  mismo nombre que componente
@@ -28,9 +31,54 @@ class Profile extends Component{
         return (
         
             <div>
-                {/* debe haber un elemento contenedor */}
-                <h5>{this.props.saludo}</h5>
-                <h3>{this.state.saludito}</h3>
+                <Container>
+                    <Row>
+                        <Col sm="12" md={{ size: 8, offset: 2 }}>
+                            <h1>Perfil</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="12" md={{ size: 8, offset: 2 }}>
+                            <Form>
+                                <FormGroup row>
+                                  <Label for="name" sm={2}>Nombre</Label>
+                                  <Col sm={10}>
+                                    <Input type="text" name="name" id="name" placeholder="Nombre" value="" />
+                                  </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                  <Label for="exampleEmail" sm={2}>Correo electrónico</Label>
+                                  <Col sm={10}>
+                                    <Input type="email" name="email" id="exampleEmail" placeholder="Correo electrónico" />
+                                  </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                  <Label for="examplePassword" sm={2}>Contraseña</Label>
+                                  <Col sm={10}>
+                                    <Input type="password" name="password" id="examplePassword" placeholder="Contraseña" />
+                                  </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                  <Label for="telephone" sm={2}>Teléfono</Label>
+                                  <Col sm={10}>
+                                    <Input type="text" name="telephone" id="telephone" placeholder="Teléfono" />
+                                  </Col>
+                                </FormGroup>
+                                <FormGroup row>
+                                  <Label for="description" sm={2}>Descripción</Label>
+                                  <Col sm={10}>
+                                    <Input type="email" name="description" id="description" placeholder="Descripción" />
+                                  </Col>
+                                </FormGroup>
+                                <FormGroup check row>
+                                  <Col sm={{ size: 2, offset: 10 }}>
+                                    <Button>Submit</Button>
+                                  </Col>
+                                </FormGroup>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </div> 
         );
     }
