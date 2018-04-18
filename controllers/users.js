31301c864e9815da79a75ba8ffd2ca1035f10ffa
Vicky <vicki.sedano@gmail.com>
2018-04-17 21:38:12 -0500

    Endpoint Login

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,6 @@
+import  jwt  from 'jsonwebtoken';
+import {SECRET_KEY} from '../config/config'
+import bcrypt  from 'bcrypt-nodejs';
 import db from '../models';
 //Asi siempre se manda a llamar a la bd
 
@@ -21,7 +24,44 @@ const createUser =  (request,response) => {
 
 }
 
+const cryptPassword = (password) => {
+    console.log("cryptPassword" + password);
+    return new Promise(function(resolve, reject) {
+      bcrypt.genSalt(10, function(err, salt) {
+        // Encrypt password using bycrpt module
+        if (err) return reject(err);
+  
+        bcrypt.hash(password, salt, null, function(err, hash) {
+          if (err) return reject(err);
+          return resolve(hash);
+        });
+      });
+    });
+}
+
+const login  = (req,res) => {
+    User.findOne({where:{email:req.body.email}}).then((user) => {
+        bcrypt.compare(req.body.password, user.password,(err, result) =>{
+            if(result){
+                let newUser = {
+                    email:user.email,
+                    name:`${user.first_name} ${user.lastname}`,
+                    exp: Math.floor(Date.now()/ 1000) + (60*60*24)
+                }
+                let  token = jwt.sign(newUser, SECRET_KEY);
+                return res.status(200).json({token:token});
+            }else{
+                return res.status(400).json({message:"Contraseña no coincide"});
+            }
+        })
+    }).catch(() =>{
+        return res.status(400).json({message:"El usuario no existe"})
+    }
+    );
+}
+
 export {
-    createUser
+    createUser,
+    login
 }
 
--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import {testApi} from '../controllers';
-import {createUser} from '../controllers/users'
-
+import {createUser, login} from '../controllers/users'
 
 const router = express.Router();
-
 
 
 router.get('/test',testApi);
@@ -12,5 +10,7 @@ router.get('/test',testApi);
 router.post('/users',createUser);
 
 router.put('/users/profile');
+
+router.post('/users/login',login);
 
 export default router;
