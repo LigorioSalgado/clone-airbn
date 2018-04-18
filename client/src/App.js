e0b7fbf23d3d2d2d37c68d2577370b8cc50eed7a
Javier <cjjj@tutanota.com>
2018-04-17 20:38:16 -0500

    Crea componente Profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/Test';
+import Profile from './components/Profile/Profile';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        <Test saludo={"Que ondita chavos!!"}/>
-        </p>
+        <Profile/>
       </div>
     );
   }
