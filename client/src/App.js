37304a50fd82d988fc0dd2ce25ad2f0e902fdeb8
LigorioSalgado <salgadolg5@hotmail.com>
2018-04-17 19:12:55 -0500

    ejemplos basicos

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <Test saludo={"Que ondita chavos!!"}/>
         </p>
       </div>
     );
