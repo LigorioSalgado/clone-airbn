a90e12ee925ec40865cf3f5722aa41e0b7497dc8
Sebastian Munoz <sebasmunoz694@gmail.com>
2018-04-17 21:39:24 -0500

    maquetado registro

--- client/src/components/Registro/Registro.js
+++ client/src/components/Registro/Registro.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormFeedback, FormText, Col, Button } from 'reactstrap';
 import './Registro.css';
 
 // Importacion para traer el componente raiz de react
@@ -7,22 +7,48 @@ import './Registro.css';
 // Creamos la clase con el mismo nombre del componente
 
 class Registro extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             saludito: "Hola prueba"
         }
+
+
     }
 
-    render(){
+    submitRegistro() {
+
+    }
+
+    render() {
         return (
             // Necesita regresar un objeto
-                <div class = "prueba">
-                    <h5>Hola mundo</h5>
-                    <h3>{this.state.saludito}</h3>
-                    <Button color="success">Danger!</Button>
-                </div>
-            
+            <div class="container">
+                <Col md={8}>
+                    <Form>
+                        <FormGroup>
+                            <Input type="email" name="email" id="email" placeholder="Dirección de correo electrónico" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Input type="text" name="first_name" id="first_name" placeholder="Nombres" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Input type="text" name="lastname" id="firstname" placeholder="Apellidos" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Input type="password" name="password" id="password" placeholder="Crea una contraseña" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Input type="password" name="password" id="password" placeholder="Vuelve a escribir tu contraseña" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Input type="text" name="phone_number" id="phone_number" placeholder="Número de teléfono" />
+                        </FormGroup>
+                        <Button color="danger" onClick={this.submitRegistro}>Regístrate</Button>{' '}
+                    </Form>
+                </Col>
+            </div>
+
         )
     }
 
