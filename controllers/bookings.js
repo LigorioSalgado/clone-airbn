f8fd772e46ec71d1d5d90c891d17d499abc91675
Vicky <vicki.sedano@gmail.com>
2018-04-23 21:05:32 -0500

    manager bookings

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -2,19 +2,10 @@ import db from '../models';
 
 const Booking = db.Booking;
 
-const getBookings = (request, response) => {
-    Booking.findAll({
-        /*where: {
-            UserId:request.body.id
-        }*/
-    }).then((bookings)=>{
-        response.json(bookings)
-    }).catch((err)=>{
-        response.status(404).json({message:"No existen reservaciones con este usuario"});
+const createBookings = () =>{
 
-    });
 }
 
 export{
-    getBookings
+    createBookings
 }
