2fba2c8ecac2399bc8bb4e05fc8d155065a3d105
Jorge MArtinez <jorgeme0996@gmail.com>
2018-04-30 19:19:10 -0500

    se agrega una llave

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -7,7 +7,7 @@ const viewBookingTravelerLogin =(req,res) => {
     getBookingTravelerLogin(req.user.id, req.params.id).then((response)=>{
         res.json(response).status(200);
     }).catch((err)=>{res.json(err).status(400)})
-
+}
 const createBooking = (req, res) =>{
     createBookingDB(req.body, req.user.id).then((response) => {
         res.json(response).status(201);
